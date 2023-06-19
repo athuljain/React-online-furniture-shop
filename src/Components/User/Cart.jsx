9df9e49f34e8e837ae1713ce6653cda56ff1f115
athuljain <athuljainkj2@gmail.com>
2023-06-19 11:41:05 +0530

    sixthday commit

--- src/Components/User/Cart.jsx
+++ src/Components/User/Cart.jsx
@@ -1,47 +1,45 @@
+
+
 import React, { useState } from "react";
 
 export default function Cart({ cartItems = [] }) {
+  const [quantities, setQuantities] = useState(cartItems.map(() => 1));
 
-  const [quantity,setQuantity]=useState(1)
+  const incrementQuantity = (index) => {
+    const newQuantities = [...quantities];
+    newQuantities[index] += 1;
+    setQuantities(newQuantities);
+  };
 
- const incrementQuantity =()=>{
-  setQuantity(quantity+1)
+  const decrementQuantity = (index) => {
+    if (quantities[index] > 1) {
+      const newQuantities = [...quantities];
+      newQuantities[index] -= 1;
+      setQuantities(newQuantities);
+    }
+  };
 
- }
-
- const decrementQuantity =()=>{
-  setQuantity(quantity-1)
-
-
- }
- const calculateTotalAmount = () => {
- }
-   
+  const calculateTotalAmount = (price, quantity) => {
+    return price * quantity;
+  };
 
   return (
     <div className="Cart">
       <h1>Cart</h1>
       {cartItems.length > 0 ? (
         <div>
-          {cartItems.map((item) => (
+          {cartItems.map((item, index) => (
             <div key={item.id} className="cart-item">
               <div className="image-container">
                 <img src={item.image} alt={item.title} style={{ width: "250px" }} />
               </div>
               <div className="item-details">
                 <h4 style={{ color: "black" }}>{item.title}</h4>
-                <p style={{ color: "black" }}>Price: {item.price}</p>
+                <p style={{ color: "black" }}>Price: {calculateTotalAmount(item.price, quantities[index])}</p>
                 <div>
-                
-                    <button onClick={() => decrementQuantity()}>-</button>
-                  
-                  <h6>Quantity: {quantity} </h6>
-                  <button onClick={() => incrementQuantity()}>+</button>
-
-                  <div className="total-amount">
-        <h4 style={{"color":"black"}}>Total Amount: {calculateTotalAmount()}</h4>
-        <button className="buy-button">Buy</button>
-      </div>
+                  <button onClick={() => decrementQuantity(index)}>-</button>
+                  <h6>Quantity: {quantities[index]} </h6>
+                  <button onClick={() => incrementQuantity(index)}>+</button>
                 </div>
               </div>
             </div>
@@ -50,8 +48,6 @@ export default function Cart({ cartItems = [] }) {
       ) : (
         <p style={{ color: "black" }}>Your cart is empty.</p>
       )}
-
-      
     </div>
   );
 }
