fb4aa2a9e87538d4226ab4a606d3b7bc955be62b
athuljain <athuljainkj2@gmail.com>
2023-12-05 09:04:01 +0530

    AdminLogin

--- src/Components/Admin/AdminLogin.jsx
+++ src/Components/Admin/AdminLogin.jsx
@@ -1,45 +1,39 @@
 import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
-import "./AdminLogin.css"
-
-
+import "./AdminLogin.css";
 
 export default function AdminLogin() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (username === "admin" && password === "admin123") {
+      navigate("/admin");
+    } else {
+      console.log("Incorrect username or password");
+    }
+  };
   
-    const handleLogin = () => {
-      if (username === 'admin' && password === 'admin123') {
-       
-        navigate("/admin")
-      } else {
-        
-  
-        console.log('Incorrect username or password');
-      }
-    };
-  
-    return (
-      <div className="admin-login-container">
-        <h1>Admin Login</h1>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button onClick={handleLogin}>Login</button>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="admin-login-container">
+      <h1>Admin Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
